ebc710b6421f2845216903d86a43376ffff9b6bc
omkark06 <omkar@zinclabs.io>
2024-06-05 17:00:24 +0530

    fix: exploring trace logs in logs explorer

--- web/src/composables/useLogs.ts
+++ web/src/composables/useLogs.ts
@@ -3286,8 +3286,8 @@ const useLogs = () => {
     }
 
     const date = {
-      startTime: queryParams.from,
-      endTime: queryParams.to,
+      startTime: Number(queryParams.from),
+      endTime: Number(queryParams.to),
       relativeTimePeriod: queryParams.period || null,
       type: queryParams.period ? "relative" : "absolute",
     };
